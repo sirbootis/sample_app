7163ee267d146379972b45cdee91feec7614a060
BG <sirbootis@gmail.com>
2014-01-11 21:05:21 -0600

    Finish user signup

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,8 @@ module ApplicationHelper
       "#{base_title} | #{page_title}"
     end
   end
+
+  def indent_output(output, indentation)
+    output.gsub("\n", "\n" + indentation).html_safe
+  end
 end
