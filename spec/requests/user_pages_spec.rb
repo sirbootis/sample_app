644b73499117db091ea65ebf4c44bd54c90bb979
BG <sirbootis@gmail.com>
2014-01-14 20:40:30 -0600

    Finish sign in

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -31,23 +31,18 @@ describe "User pages" do
     end
 
     describe "with valid information" do
-      before do
-        fill_in "Name",         with: "Example User"
-        fill_in "Email",        with: "user@example.com"
-        fill_in "Password",     with: "foobar"
-        fill_in "Confirmation", with: "foobar"
-      end
+      let(:user) { FactoryGirl.build(:user) }
 
       it "should create a user" do
-        expect { click_button submit }.to change(User, :count).by(1)
+        expect { valid_signup(user) }.to change(User, :count).by(1)
       end
 
       describe "after saving the user" do
-        before { click_button submit }
-        let(:user) { User.find_by(email: 'user@example.com') }
+        before { valid_signup(user) }
 
+        it { should have_link('Sign out') }
         it { should have_title(user.name) }
-        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+        it { should have_success_message('Welcome') }
       end
     end
   end
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,1 +1,27 @@
 include ApplicationHelper
+
+def valid_signin(user)
+  fill_in "Email",    with: user.email
+  fill_in "Password", with: user.password
+  click_button "Sign in"
+end
+
+def valid_signup(user)
+  fill_in "Name",         with: user.name
+  fill_in "Email",        with: user.email
+  fill_in "Password",     with: user.password
+  fill_in "Confirmation", with: user.password_confirmation
+  click_button submit
+end
+
+RSpec::Matchers.define :have_error_message do |message|
+  match do |page|
+    expect(page).to have_selector('div.alert.alert-error', text: message)
+  end
+end
+
+RSpec::Matchers.define :have_success_message do |message|
+  match do |page|
+    expect(page).to have_selector('div.alert.alert-success', text: message)
+  end
+end
